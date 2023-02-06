d017eec3b36931734ff0b81e661f9a9f8e4bc26a
Adhnan Shereef T <93500202+AdhnanShereef@users.noreply.github.com>
2023-02-06 14:36:44 +0530

    Home Template Settings Changed

--- home/views.py
+++ home/views.py
@@ -6,17 +6,17 @@ from .models import Tweet
 def home(request):
     context = {'title': ''}
     if request.user.is_authenticated:
-        context['title']='Home'
-        return render(request, 'home/home.html', context)
+        context['title'] = 'Home'
+        return render(request, 'home/home/home.html', context)
     else:
-        context['title']='Explore'
-        return render(request, 'home/home.html', context)
+        context['title'] = 'Explore'
+        return render(request, 'home/home/home.html', context)
+
 
 def tweet(request):
-    if request.method=='POST' and request.user.is_authenticated :
-        tweet = Tweet.objects.create(user=request.user, content=request.POST.get('content'))
+    if request.method == 'POST' and request.user.is_authenticated:
+        tweet = Tweet.objects.create(
+            user=request.user, content=request.POST.get('content'))
         tweet.save()
         return redirect('home')
     return redirect('home')
-
-    

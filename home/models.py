7bd3720f0fdee4f1164a49de39966b93bbb8ba74
Adhnan Shereef T <93500202+AdhnanShereef@users.noreply.github.com>
2023-02-07 18:11:56 +0530

    Home tweet view's design is ready, need add actions, there is a change in tweet model

--- home/models.py
+++ home/models.py
@@ -14,20 +14,35 @@ class TweetReply(models.Model):
     retweet = models.IntegerField(default=0)
     views = models.IntegerField(default=0)
 
+
+class TweetViews(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    impression = models.IntegerField(default=0)
+    engagements = models.IntegerField(default=0)
+    detail_expands = models.IntegerField(default=0)
+    new_follower = models.IntegerField(default=0)
+    profile_visits = models.IntegerField(default=0)
+
 # Main Tweet Model
+
+
 class Tweet(models.Model):
+    id = models.BigAutoField(primary_key=True, unique=True)
     content = models.TextField(null=True, max_length=500)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     images = models.ManyToManyField(
         TweetImage, related_name='images', blank=True)
-    like = models.IntegerField(default=0)
+    like = models.ManyToManyField(
+        User, related_name='like', blank=True)
     reply = models.ManyToManyField(
         TweetReply, related_name='comments', blank=True)
-    reply_count = models.IntegerField(default=0)
-    retweet = models.IntegerField(default=0)
-    views = models.IntegerField(default=0)
+    retweet = models.ManyToManyField(
+        User, related_name='retweet', blank=True)
+    views = models.ManyToManyField(
+        TweetViews, related_name='views', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
     class Meta:
         ordering = ['-created']
 
